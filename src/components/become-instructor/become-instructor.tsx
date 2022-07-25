e22bd3dcd04bc756da4d3f2505a9b40128a3d8c5
mahdihasanzade-dev <hasanzadehmahdi66@gmail.com>
2022-07-25 11:53:27 +0430

    complete become a instructor section

---
+++ src/components/become-instructor/become-instructor.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@components/button/button';
+import instrucorImg from '@assets/img/bai-img.png';
+import './become-instructor.scss';
+
+export function BecomeInstructor() {
+  return (
+    <section className="bai-section section-padding">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-cl-10">
+            <div className="box">
+              <div className="row align-items-center">
+                <div className="col-md-6">
+                  <div className="circular-img">
+                    <div className="circular-img__inner">
+                      <div className="circular-img__circle" />
+                      <img src={instrucorImg} alt="become instructor" />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="section-title">
+                    <p className="sub-title">مدرس شوید</p>
+                    <h2 className="title">اگر علاقه مند به تولید محتوا هستید، با ما در ارتباط باشید</h2>
+                  </div>
+                  <div className="col-12 mt-3 d-flex justify-content-center">
+                    <Button
+                      text="ثبت درخواست"
+                      onClick={() => console.log('all course')}
+                      backgroundColor="main"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HeroSection } from '@components/hero-section/hero-section';
 import { Facts } from '@components/facts/facts';
 import { CoursesSection } from '@components/courses-section/courses-section';
 import { Testimonials } from '@components/testimonials/testimonials';
+import { BecomeInstructor } from '@components/become-instructor/become-instructor';
 import './globals/bootstrap.min.css';
 import './globals/index.scss';
 
@@ -14,5 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Facts />
     <CoursesSection />
     <Testimonials />
+    <BecomeInstructor />
   </div>,
 );
